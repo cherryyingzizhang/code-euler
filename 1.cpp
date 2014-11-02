b8e6149060d5237482cf1f4d286d6cdd935c8f9a
phantomkirby <shadowingphantom@gmail.com>
2014-11-02 23:41:41 +0900

    first commit

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+   int sum = 0;
+   
+   for (int i = 0; i < 1000; i++)
+   {
+       if (i%3==0)
+       {
+           sum += i;
+       }
+       else if (i%5==0)
+       {
+           sum += i;
+       }
+   }
+   cout << sum;
+   return 0;
+}
